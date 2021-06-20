2de46642921899223b85d049cd8e7ec5ec182529
Tatsuya Nakajima <60083659+NakajimaTatusya@users.noreply.github.com>
2021-06-21 00:02:40 +0900

    IEのお気に入りからEdgeへ

---
+++ ie_fav_to_edge.ps1
@@ -0,0 +1,28 @@
+## Internet Explorer お気に入りパスを取得
+[STRING]$IEpath = $([Environment]::GetFolderPath("Favorites"))
+ 
+## Microsoft Edge お気に入りパス
+[STRING]$EdgePath = $($env:LOCALAPPDATA + "\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\AC\MicrosoftEdge\User\Default\Favorites")
+ 
+## お気に入りレジストリパス
+[string]$FavOrder = "HKCR:\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\FavOrder"
+ 
+## パスの存在確認
+IF((Test-Path -Path $IEpath) -and (Test-Path -Path $EdgePath)) {
+ 
+        ## Internet Explorer から Edgeへお気に入りをコピーする
+        Copy-Item -Path "$IEpath\*" -Destination $EdgePath -recurse -container -force -Exclude "`$RECYCLE.BIN" -ErrorAction SilentlyContinue
+ 
+        ## HKEY_CLASSES_ROOTのマウント
+        New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
+ 
+        ## レジストリキーFavOrderが有効かどうかを確認
+        IF(Test-Path -Path $FavOrder)
+            {
+                # FavOrderをレジストリから削除
+                Remove-Item -Path $FavOrder -Recurse
+            }
+ 
+        ## マウント解除
+        Remove-PSDrive -Name HKCR
+    }
