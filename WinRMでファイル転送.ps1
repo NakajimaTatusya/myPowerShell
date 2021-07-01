06ddd89054ce0295e12df68f5ed7b779b2175b2c
Tatsuya Nakajima <60083659+NakajimaTatusya@users.noreply.github.com>
2021-07-01 15:34:28 +0900

    addnew

---
+++ WinRMでファイル転送.ps1
@@ -0,0 +1,5 @@
+﻿$cred = Get-Credential -UserName Administrator -Message "Administratorのパスワード"
+$hostname = "192.168.1.202"
+$session = New-PSSession –ComputerName $hostname -Credential $cred
+Copy-Item -Path 'C:\Users\hogehoge\Documents\debug.log' -Destination 'C:\tmp\' -ToSession $session
+$session | Remove-PSSession
