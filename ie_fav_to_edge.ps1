4550bf6c991cabf7a946c57f5aa1fd412a259ab8
Tatsuya Nakajima <60083659+NakajimaTatusya@users.noreply.github.com>
2021-07-28 11:56:39 +0900

    インデント修正

--- ie_fav_to_edge.ps1
+++ ie_fav_to_edge.ps1
@@ -8,21 +8,21 @@
 [string]$FavOrder = "HKCR:\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\FavOrder"
  
 ## パスの存在確認
-IF((Test-Path -Path $IEpath) -and (Test-Path -Path $EdgePath)) {
+if ((Test-Path -Path $IEpath) -and (Test-Path -Path $EdgePath)) {
  
-        ## Internet Explorer から Edgeへお気に入りをコピーする
-        Copy-Item -Path "$IEpath\*" -Destination $EdgePath -recurse -container -force -Exclude "`$RECYCLE.BIN" -ErrorAction SilentlyContinue
- 
-        ## HKEY_CLASSES_ROOTのマウント
-        New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
- 
-        ## レジストリキーFavOrderが有効かどうかを確認
-        IF(Test-Path -Path $FavOrder)
-            {
-                # FavOrderをレジストリから削除
-                Remove-Item -Path $FavOrder -Recurse
-            }
- 
-        ## マウント解除
-        Remove-PSDrive -Name HKCR
+    ## Internet Explorer から Edgeへお気に入りをコピーする
+    Copy-Item -Path "$IEpath\*" -Destination $EdgePath -recurse -container -force -Exclude "`$RECYCLE.BIN" -ErrorAction SilentlyContinue
+
+    ## HKEY_CLASSES_ROOTのマウント
+    New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
+
+    ## レジストリキーFavOrderが有効かどうかを確認
+    if (Test-Path -Path $FavOrder)
+    {
+        # FavOrderをレジストリから削除
+        Remove-Item -Path $FavOrder -Recurse
     }
+
+    ## マウント解除
+    Remove-PSDrive -Name HKCR
+}
