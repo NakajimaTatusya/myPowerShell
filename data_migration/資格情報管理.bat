63d636d89252e877231393fda34373e771dff913
Tatsuya Nakajima <60083659+NakajimaTatusya@users.noreply.github.com>
2021-08-06 15:27:56 +0900

    資格情報管理画面呼び出しバッチファイル追加

---
+++ data_migration/資格情報管理.bat
@@ -0,0 +1,6 @@
+@echo off
+REM https://superuser.com/questions/1357277/how-can-i-backup-the-windows-web-credentials-store
+rundll32.exe keymgr.dll, KRShowKeyMgr
+
+rem cmdkey.exe
+rem https://docs.microsoft.com/ja-jp/windows-server/administration/windows-commands/cmdkey
