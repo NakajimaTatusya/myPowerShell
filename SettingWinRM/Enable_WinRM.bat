a8d4940d823d90528fd66744211921e62bdcff36
Tatsuya Nakajima <60083659+NakajimaTatusya@users.noreply.github.com>
2021-09-09 13:42:46 +0900

    add winrm show config

--- SettingWinRM/Enable_WinRM.bat
+++ SettingWinRM/Enable_WinRM.bat
@@ -13,5 +13,7 @@ powershell -Command "Get-ExecutionPolicy -List"
 
 powershell -NoProfile -Command "&{Start-Process powershell -ArgumentList '-noprofile -file %CURRENTDIR%\Enable-WinRM-for-WIn10.ps1 -currentDir %CURRENTDIR%' -Verb runas}"
 
+winrm get winrm/config
+
 pause
 exit 0
